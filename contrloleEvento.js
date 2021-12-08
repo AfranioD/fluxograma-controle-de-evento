87d686e0684f612916302d219c88218da3ed1199
Afranio <afraniodimasdasilva@gmail.com>
2021-12-07 22:06:49 -0300

    Adicionado arquivo.js

---
+++ contrloleEvento.js
@@ -0,0 +1,26 @@
+var controleDeEvento = ["Controle de Evento"]
+console.log("Controle de Evento");
+
+// validando evento
+var dataDoEvento =["Entre com a data do evento"]
+var dataDoEvento =10/06/2021
+if(dataDoEvento.length < 10/06/2021){
+  console.logo("Evento não Permitido")
+}else{
+  console.log("Evento Permitido")
+}
+
+//validando idade
+let idade = 18
+if(idade <= 18){
+  console.log("Idade não permitida para Cadastramento")
+}else{
+  console.log
+  ("Cadastro Permitido")
+}
+
+//Controle de inscritos
+let quantidadeDeCadastro = 100
+if (quantidadeDeCadastro <= 100){
+  console.log ("Cadastrar")
+  }else {console.log ("Numero de Cadastro excedeu o limite permitido")}
